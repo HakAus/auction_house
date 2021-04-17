2cd30a2ef63ed17783f42ecc6ba40a0bd671e99e
Fernando Alvarez <ferolsen@hotmail.com>
2021-04-17 13:15:33 -0600

    Update Registrar Subastas

--- oracle/Scripts/registrarSubasta.sql
+++ oracle/Scripts/registrarSubasta.sql
@@ -1,16 +1,22 @@
 
-CREATE OR REPLACE PROCEDURE AUCT_ITEM(IdSubasta NUMBER,IdItemSubastado NUMBER,IdVendedor NUMBER,FechaHoraCierre TIMESTAMP,DetallesDeEntrega VARCHAR2,pIdItem NUMBER,pIdSubcategoria NUMBER,pPrecioBase NUMBER,pDescripcion VARCHAR2,pImagen BLOB) AS 
-CURSOR search_item IS
-SELECT idItem FROM Items WHERE idItem = pIdItem;
-CURSOR search_subasta IS
-SELECT idItem FROM Items WHERE idItem = pIdItem;
+CREATE OR REPLACE PROCEDURE AUCT_ITEM(pIdSubasta NUMBER,IdItemSubastado NUMBER,IdVendedor NUMBER,FechaHoraCierre TIMESTAMP,DetallesDeEntrega VARCHAR2,pIdItem NUMBER,pIdSubcategoria NUMBER,pPrecioBase NUMBER,pDescripcion VARCHAR2,pImagen BLOB) AS 
+itemsEncontrados Items%ROWTYPE;
+subastasEncontradas Subastas%ROWTYPE;
 BEGIN
-    OPEN search_Item;    
-    IF search_Item%NOTFOUND THEN
-        INSERT INTO Items VALUES(pIdItem,pIdSubcategoria,pPrecioBase,pDescripcion,pImagen);
-        OPEN search_subasta;
-        IF search_subasta%NOTFOUND THEN
-            INSERT INTO Subastas VALUES(IdSubasta,IdItemSubastado,IdVendedor,FechaHoraCierre,DetallesDeEntrega);
+    SELECT * INTO itemsEncontrados FROM Items WHERE idItem = pIdItem;  
+    EXCEPTION
+      WHEN NO_DATA_FOUND THEN
+        itemsEncontrados := NULL;
+    IF SQL%NOTFOUND THEN
+        BEGIN
+            INSERT INTO Items VALUES(pIdItem,pIdSubcategoria,pPrecioBase,pDescripcion,pImagen);
+            SELECT * INTO subastasEncontradas FROM Subastas WHERE idSubasta = pIdSubasta; 
+            EXCEPTION
+                WHEN NO_DATA_FOUND THEN
+                    subastasEncontradas := NULL;
+        END;
+        IF SQL%NOTFOUND THEN
+            INSERT INTO Subastas VALUES(pIdSubasta,IdItemSubastado,IdVendedor,FechaHoraCierre,DetallesDeEntrega);
         END IF;
     END IF;
 END;
