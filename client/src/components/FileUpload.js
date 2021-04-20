df2356f9e88d375dbd467e09a3f3a99a78bf436b
Xfighter32 <austinhakanson2@gmail.com>
2021-04-20 14:25:18 -0600

    Actualización del formulario para agregar una subasta

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,69 +1,106 @@
-import React, { useState } from 'react'
-import Dropzone from 'react-dropzone';
-import Icon from '@ant-design/icons';
+import React, { useState } from "react";
+import Dropzone from "react-dropzone";
+import Icon from "@ant-design/icons";
 function FileUpload(props) {
+  //Hooks
+  const [Images, setImages] = useState([]);
 
-    const [Images, setImages] = useState([])
+  // Functions
+  const onDrop = (files) => {
+    let formData = new FormData();
+    const config = {
+      header: { "content-type": "multipart/form-data" },
+    };
+    formData.append("file", files[0]);
+    //save the Image we chose inside the Node Server
+    setImages([...Images, files[0].image]);
+    props.refreshFunction([...Images, files[0].image]);
+  };
 
-    const onDrop = (files) => {
+  const onDelete = (image) => {
+    const currentIndex = Images.indexOf(image);
 
-        let formData = new FormData();
-        const config = {
-            header: { 'content-type': 'multipart/form-data' }
-        }
-        formData.append("file", files[0])
-        //save the Image we chose inside the Node Server 
-        setImages([...Images, files[0].image])
-        props.refreshFunction([...Images, files[0].image])
-    }
+    let newImages = [...Images];
+    newImages.splice(currentIndex, 1);
 
+    setImages(newImages);
+    props.refreshFunction(newImages);
+  };
 
-    const onDelete = (image) => {
-        const currentIndex = Images.indexOf(image);
-
-        let newImages = [...Images]
-        newImages.splice(currentIndex, 1)
-
-        setImages(newImages)
-        props.refreshFunction(newImages)
-    }
-
-    return (
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Dropzone
-                onDrop={onDrop}
-                multiple={false}
-                maxSize={800000000}
+  // JSX
+  return (
+    // Contenedor principal
+    <div
+      style={{
+        marginTop: 15,
+        marginBottom: 15,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      {/* Título */}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "left",
+        }}
+      >
+        <label style={{ textAlign: "left" }}>Imagen del item</label>
+      </div>
+      {/* Sección de la imagen */}
+      <div
+        style={{
+          marginTop: 15,
+          marginBottom: 15,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Dropzone onDrop={onDrop} multiple={false} maxSize={800000000}>
+          {({ getRootProps, getInputProps }) => (
+            <div
+              style={{
+                width: "300px",
+                height: "240px",
+                border: "1px solid lightgray",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              {...getRootProps()}
             >
-                {({ getRootProps, getInputProps }) => (
-                    <div style={{
-                        width: '300px', height: '240px', border: '1px solid lightgray',
-                        display: 'flex', alignItems: 'center', justifyContent: 'center'
-                    }}
-                        {...getRootProps()}
-                    >
-                        {console.log('getRootProps', { ...getRootProps() })}
-                        {console.log('getInputProps', { ...getInputProps() })}
-                        <input {...getInputProps()} />
-                        <Icon type="plus" style={{ fontSize: '3rem' }} />
-
-                    </div>
-                )}
-            </Dropzone>
-
-            <div style={{ display: 'flex', width: '350px', height: '240px', overflowX: 'scroll' }}>
-
-                {Images.map((image, index) => (
-                    <div onClick={() => onDelete(image)}>
-                        <img style={{ minWidth: '300px', width: '300px', height: '240px' }} src={`http://localhost:5000/${image}`} alt={`productImg-${index}`} />
-                    </div>
-                ))}
-
-
+              {console.log("getRootProps", { ...getRootProps() })}
+              {console.log("getInputProps", { ...getInputProps() })}
+              <input {...getInputProps()} />
+              <Icon type="plus" style={{ fontSize: "3rem" }} />
             </div>
-
+          )}
+        </Dropzone>
+        {/* Espacio extra a la derecha */}
+        <div
+          style={{
+            display: "flex",
+            width: "350px",
+            height: "240px",
+            overflowX: "scroll",
+          }}
+        >
+          {Images.map((image, index) => (
+            <div onClick={() => onDelete(image)}>
+              <img
+                style={{ minWidth: "300px", width: "300px", height: "240px" }}
+                src={`http://localhost:5000/${image}`}
+                alt={`productImg-${index}`}
+              />
+            </div>
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default FileUpload
+export default FileUpload;
--- client/src/views/CreateAuctionView.js
+++ client/src/views/CreateAuctionView.js
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Typography, Button, Form, Input } from "antd";
+import {
+  Menu,
+  Dropdown,
+  TimePicker,
+  DatePicker,
+  Typography,
+  Button,
+  Form,
+  Input,
+} from "antd";
 import FileUpload from "../components/FileUpload";
 import Icon from "@ant-design/icons";
 
@@ -8,13 +17,26 @@ const { Title } = Typography;
 const { TextArea } = Input;
 
 const CreateAuctionView = (setAuth) => {
+  // Hooks
   const [TitleValue, setTitleValue] = useState("");
   const [DescriptionValue, setDescriptionValue] = useState("");
   const [PriceValue, setPriceValue] = useState(0);
   const [ContinentValue, setContinentValue] = useState(1);
-
   const [Images, setImages] = useState([]);
 
+  const [inputs, setInputs] = useState({
+    cedula: "",
+    tipo_usuario: "administrador",
+    alias: "",
+    contrasena: "",
+    nombre: "",
+    primer_apellido: "",
+    segundo_apellido: "",
+    direccion: "",
+    correo: "",
+  });
+
+  // Functions
   const onTitleChange = (event) => {
     setTitleValue(event.currentTarget.value);
   };
@@ -34,6 +56,7 @@ const CreateAuctionView = (setAuth) => {
   const updateImages = (newImages) => {
     setImages(newImages);
   };
+
   const onSubmit = (event) => {
     event.preventDefault();
 
@@ -67,39 +90,86 @@ const CreateAuctionView = (setAuth) => {
             })*/
   };
 
+  // Opciones de los dropdown
+  const opcionesCategoria = (
+    <Menu>
+      <Menu.Item key="0">
+        <a href="https://www.antgroup.com">1st menu item</a>
+      </Menu.Item>
+      <Menu.Item key="1">
+        <a href="https://www.aliyun.com">2nd menu item</a>
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item key="3">3rd menu item</Menu.Item>
+    </Menu>
+  );
+
+  // JSX
   return (
-    <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+    <div style={{ maxWidth: "70%", margin: "2rem auto", textAlign: "left" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
         <Title level={2}> Agregar Subasta</Title>
       </div>
-
+      <div style={{ textAlign: "left", marginBottom: "2rem" }}>
+        <Title level={4}> Detalles del item</Title>
+      </div>
       <Form onSubmit={onSubmit}>
         {/* DropZone */}
         <FileUpload refreshFunction={updateImages} />
-
-        <br />
-        <br />
-        <label>Nombre</label>
+        <label style={{ marginVertical: 15 }}>Nombre</label>
         <Input onChange={onTitleChange} value={TitleValue} />
+        {/* Seleccionadores de categoria y subcategoria */}
         <br />
         <br />
-        <label>Description</label>
+        <table style={{ width: "100%", tableLayout: "fixed" }}>
+          <td>
+            <tr style={{ alignItems: "center" }}>
+              <label>Categoría</label>
+            </tr>
+            <tr>
+              <Dropdown overlay={opcionesCategoria}>
+                <Button>Seleccione la categoría</Button>
+              </Dropdown>
+            </tr>
+          </td>
+          <td>
+            <tr>
+              <label style={{ marginVertical: 15 }}>Subcategoría</label>
+            </tr>
+            <tr>
+              <Dropdown overlay={opcionesCategoria}>
+                <Button>Seleccione la subcategoría</Button>
+              </Dropdown>
+            </tr>
+          </td>
+        </table>
+        <br />
+        <br />
+        <label>Descripción</label>
         <TextArea onChange={onDescriptionChange} value={DescriptionValue} />
         <br />
         <br />
-        <label>Precio($)</label>
+        <label>Precio(₡)</label>
         <Input onChange={onPriceChange} value={PriceValue} type="number" />
         <br />
         <br />
-        <select onChange value={"CategoriaTest"}>
-          <option key={"item.key"} value={"item.key"}>
-            {"Categoria test"}{" "}
-          </option>
-        </select>
+        <label>Fecha de cierre</label>
+        <br />
+        <DatePicker
+          style={{ width: 300 }}
+          format="DD/MM/YYYY"
+          placeholder="Seleccione una fecha"
+        />
         <br />
         <br />
-
-        <Button onClick={onSubmit}>Subastar</Button>
+        <label>Hora de cierre</label>
+        <br />
+        <TimePicker style={{ width: 300 }} placeholder="Seleccione una hora" />
+        <br />
+        <br />
+        <Button style={{ width: "100%" }} onClick={onSubmit}>
+          Subastar
+        </Button>
       </Form>
     </div>
   );
